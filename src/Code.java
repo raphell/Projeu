dc28e83890176988cced9d0816a55e7990e69260
lucianor <lucianor@PC-A102-14.iut-valence.fr>
2016-03-07 16:30:49 +0100

    # WARNING: head commit changed in the meantime
    new version 0

---
+++ src/Code.java
@@ -0,0 +1,19 @@
+
+/**
+ * 
+ * @author lucianor
+ *
+ */
+public class Code {
+	
+	pions x1,x2,x3,x4 ;
+
+	/**
+	 * Creates a random code
+	 */
+	public Code()
+	{
+		
+	}
+
+}
--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,47 @@
-
-public class Game { /*represent a game of MasterMind*/
+/**
+ * A MasterMind game
+ */
+public class Game { 
 	
-	Game(){
+	final static int NBR_TOURS= 12 ;
+
+	private final Code code;
+	
+	/**
+	 * Creates a new Mastermind game, with random code
+	 */
+	public Game()
+	{
+		this.code = new Code();
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public void play()
+	{
+		int i = 1 ;	
+		while(i<NBR_TOURS){
+		
+		
+		
+		}
+	}
+	
+	
+	/**
+	 * a pawn
+	 */
+	public class pawn
+	{
+		final int R ;
+		final int G ;
+		final int B ;
+		
+		void affichepion(int x, int y)
+		{
 			
+		}
 	}
 }
